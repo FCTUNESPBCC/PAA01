0d11e76665283ffee0ddc82a994497248c12b1fb
Leonardo Dias <leonardo.dias@multimidiaeducacional.com.br>
2015-01-19 11:25:33 -0200

    Classe Arquivo

--- src/classes/Arquivo.java
+++ src/classes/Arquivo.java
@@ -1,24 +1,58 @@
 package classes;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Arquivo {
 
 	private static File arquivo;
+	private static BufferedWriter buffWrite;
+	private static BufferedReader buffReader;
 	
-	public static void abreArquivo(){
-		
+	public static void abreArquivoEscrita() throws IOException{
+		if(!arquivo.exists())
+			arquivo.createNewFile();
+		buffWrite = new BufferedWriter(new FileWriter(arquivo.getAbsoluteFile()));
 	}
 	
-	public static void fechaArquivo(){
-		
+	public static void fechaArquivoEscrita() throws IOException{
+		buffWrite.close();
 	}
 	
-	public static void setArquivo(File arquivo){
-		Arquivo.arquivo = arquivo;
+	public static void abreArquivoLeitura() throws IOException{
+		if(!arquivo.exists())
+			arquivo.createNewFile();
+		buffReader = new BufferedReader(new FileReader(arquivo.getAbsoluteFile()));
 	}
 	
-	public static void getElements(int vetor[], int maxElem){
-		
+	public static void fechaArquivoLeitura() throws IOException{
+		buffReader.close();
 	}
+	
+	public static void setArquivo(String arquivo){
+		Arquivo.arquivo = new File(arquivo);
+	}
+	
+	public static void getElements(long vetor[], int maxElem) throws IOException{
+		abreArquivoLeitura();
+		for(int j = 0; j < 10; j++)
+			for(int i = 0; i < 50000; i++)
+				vetor[j*50000+i] = Long.parseLong(readLine());	
+		fechaArquivoLeitura();
+	}
+	
+	public static void escreve(String text) throws IOException{
+		buffWrite.write(text);
+	}
+	
+	public static String readLine() throws IOException{
+		if(buffReader.ready())
+			return buffReader.readLine();
+		return "";
+	}
+	
 }
